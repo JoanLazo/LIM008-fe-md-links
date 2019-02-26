4da89196eccdc593a3e5660383148b305af9ee24
JoanLazo <joan162581@gmail.com>
2019-02-25 19:04:10 -0500

    Se modifico los test y se creo la funcion para validar si la ruta es absoluta o relativa

--- src/controller.js
+++ src/controller.js
@@ -1,2 +1,10 @@
+/**
+  * 
+  * @param {Evalua si la ruta es absoluta o relativa} path 
+  */
 
 // path.isAbsolute()
+export const pathAbsolute = (root) => {
+ const isAbsolute = path.isAbsolute(root); 
+ return isAbsolute;
+}
--- test/controller.spec.js
+++ test/controller.spec.js
@@ -1,45 +1,46 @@
-import {  pathAbsolute, countPath, extractMdFiles, extractLinks } from '../src/controller.js'
+import { pathAbsolute, countPath, extractMdFiles, extractLinks } from '../src/controller.js';
 
-const input = {
-    path: 'C:\Users\ivan_\Desktop\PROYECTO MARKDOWN\LIM008-fe-md-links',
-    options: '', 
-};
+// const input = {
+//     path: 'C:\Users\ivan_\Desktop\PROYECTO MARKDOWN\LIM008-fe-md-links',
+//     options: '', 
+// };
+
+
 describe('Ejecuta la libreria con una ruta y puede colocar opciones', () => {
-    it('Valida si es una ruta absoluta', (done) => {
-        return pathAbsolute('C:\Users\ivan_\Desktop\PROYECTO MARKDOWN\LIM008-fe-md-links')
-        .then( () => getPath( 
-         (obj) => {
-           const result = obj.find( (route) => route.path === 'C:\Users\ivan_\Desktop\PROYECTO MARKDOWN\LIM008-fe-md-links');
-           expect(result.path).toBe(true);
-           done();
-        }));
-    });
-    it('Encuentra la cantidad de archivos', (done) => {
-    return countPath('C:\Users\ivan_\Desktop\PROYECTO MARKDOWN\LIM008-fe-md-links','')
-    .then( () => ( 
-     (path) => {
-       const result = path.find( (route) => route.path === 'C:\Users\ivan_\Desktop\PROYECTO MARKDOWN');
-       expect(result.path).toBe(['C:\Users\ivan_\Desktop\PROYECTO MARKDOWN\LIM008-fe-md-links']);
-       done();
-    }));
-});
-    it('Extrae los archivos .md de las rutas', (done) => {
-    return extractMdFiles('C:\Users\ivan_\Desktop\PROYECTO MARKDOWN\LIM008-fe-md-links','')
-    .then( () => ( 
-     (path) => {
-       const result = path.find( (route) => route.file === 'README.md');
-       expect(result.file).toBe('README.md');
-       done();
-    }));
+  it('Valida si es una ruta absoluta', () => {
+    return expect(typeof pathAbsolute).toBe('función');
+  });
+  it('Deberia retornar true si la ruta es absoluta', () => {
+    expect(pathAbsolute('C:\Users\ivan_\Desktop\PROYECTO MARKDOWN\LIM008-fe-md-links')).toBe(true);
+  });
+  it('Deberia retornar true si la ruta es absoluta', () => {
+    expect(pathAbsolute('LIM008-fe-md-links')).toBe(false);
+  });
+}); 
+
+describe('Abre directorios hasta encontrar archivos', () => {
+  it('Encuentra la cantidad de archivos', () => {
+    return expect(typeof countPath).toBe('función');
+  });
+  it('')
+}); 
+
+it('Extrae los archivos .md de las rutas', (done) => {
+  return extractMdFiles('C:\Users\ivan_\Desktop\PROYECTO MARKDOWN\LIM008-fe-md-links', '')
+    .then(() => ( 
+      (path) => {
+        const result = path.find((route) => route.file === 'README.md');
+        expect(result.file).toBe('README.md');
+        done();
+      }));
 });
 it('Extraer los links con un largo menos a 50 caracteres de los archivos .md', (done) => {
-    return extractLinks('README.md')
-    .then( () => ( 
-     (path) => {
-       const result = path.find( (route) => route.links === 'https://es.wikipedia.org/wiki/Markdown');
-       expect(result.links).toBe('https://es.wikipedia.org/wiki/Markdown');
-       done();
-    }));
+  return extractLinks('README.md')
+    .then(() => ( 
+      (path) => {
+        const result = path.find((route) => route.links === 'https://es.wikipedia.org/wiki/Markdown');
+        expect(result.links).toBe('https://es.wikipedia.org/wiki/Markdown');
+        done();
+      }));
 });
-}); 
    
