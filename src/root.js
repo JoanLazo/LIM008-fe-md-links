0832272d0cac2cd7edbcacd6596cca520e8c8f5b
Ivan <ivanlazo579@gmail.com>
2019-03-04 10:38:35 -0500

    Modificando la función de readFiles

--- src/root.js
+++ src/root.js
@@ -4,6 +4,9 @@
   */
 const path = require('path');
 const fs = require('fs');
+const marked = require('marked');
+const jsdom = require(' jsdom ');
+const { JSDOM } = jsdom;
 
 // path.isAbsolute() retorna un boleano
 export const pathAbsolute = (root) => {
@@ -16,7 +19,7 @@ export const convertInAbsolute = (root) => {
   const pathResolve = path.resolve(root);
   return pathResolve;
 };
-
+// `${process.cwd(path)}/${paths.basename(path)}`; 
 // dirent.isDirectory () retorna un bolean, dirent.isFile () retorna un boleano
 // stats.isDirectory () Devuelve true si el fs.Statsobjeto describe un directorio de sistema de archivos, stats.isFile().
 // fs.readdirSync(path[, options]) devuelve string o un directorio o buffer
@@ -42,20 +45,22 @@ console.log(isDirOrFile('C:\\Users\\ivan_\\Desktop\\PROYECTO MARKDOWN\\LIM008-fe
 // console.log(isDirOrFile('C:\\Users\\Laboratoria\\Documents\\PROYECTO MARKDOWN\\LIM008-fe-md-links\\test\\prueba'));
 // path.extname() retorna una cadena
 
-export const readFiles = (root, options) => {  
-  const readMdFiles = fs.readFileSync(root, options);
-  return readMdFiles;
+export const readFilesMd = (arrFiles) => { 
+  const links = []; 
+  arrFiles.forEach( file => {
+  const readMdFiles = fs.readFileSync(file, 'utf8');
+  links.push(readMdFiles);
+  });
+  return links;
 };
 console.log(readFiles('C:\\Users\\Laboratoria\\Documents\\PROYECTO MARKDOWN\\LIM008-fe-md-links\\test\\prueba\\hijo\\file.md'));
 
 // marked(markdownString [,options] [,callback])
 // Create reference instance
-const myMarked = require('marked');
-const jsdom = require(' jsdom ');
-const { JSDOM } = jsdom;
+
 
 myMarked.setOptions({
-  renderer: new myMarked.Renderer(),
+  renderer: new marked.Renderer(),
   highlight: function(code) {
     return require('highlight.js').highlightAuto(code).value;
   },
@@ -72,6 +77,13 @@ myMarked.setOptions({
 console.log(myMarked('I am using __markdown__.'));
 
 export const convertInHtml = (root) => {
-  const markedJs = myMarked(root);
+  const markedJs = new marked.Renderer(root);
   return markedJs;
 };
+
+export  const extractLinks = (html, root) => {
+  const jsDom = new JSDON(html);
+  let objLinks = {};
+  let arrObjLinks = [];
+  const 
+}
